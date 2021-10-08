e99eb605d9d8a777e2734e22af97f2a638bde983
Chandan <chandanmohanty11111>
2021-10-08 09:21:04 +0530

    Added : Create Generic Class

---
+++ src/com/company/FindMaximum.java
@@ -0,0 +1,45 @@
+package com.company;
+
+
+public class FindMaximum <T extends Comparable> {
+    T var1, var2, var3;
+
+    public FindMaximum(T var1, T var2, T var3) {
+        this.var1 = var1;
+        this.var2 = var2;
+        this.var3 = var3;
+    }
+
+
+
+    public static void  main(String[] args) {
+        System.out.println("Welcome to Generic Program.");
+
+        Integer a = 21, b = 42, c =67;
+        Float x = 3.5f, y =9.8f , z = 5.8f;
+        String l = "Apple" , m = "Banana" , n = "Orange";
+
+        new FindMaximum<>(a,b,c).max();
+        new FindMaximum<>(x,y,z).max();
+        new FindMaximum<>(l,m,n).max();
+    }
+
+    private void max() {
+        FindMaximum.getMaximum(var1,var2,var3);
+
+    }
+
+    private static <T extends Comparable> void getMaximum(T a, T b, T c) {
+
+        T max = a;
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+
+        System.out.println("Maximum Value :: "+max);
+
+    }
+}
